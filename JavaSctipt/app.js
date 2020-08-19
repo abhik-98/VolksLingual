3a5cb0c3c0de1df815690498874fd108cdf9dc4b
abhik47 <abhishek9817@gmail.com>
2020-08-19 14:18:38 +0530

    FINAL

--- JavaSctipt/app.js
+++ JavaSctipt/app.js
@@ -1,14 +1,26 @@
 $(function () {
 	$('#myButton').floatingWhatsApp({
 		phone: '+917058684314',
-		popupMessage: 'Hello, how can we help you?',
+		popupMessage: 'Hello, how can I help you?',
 		message: "I'd like to join ",
 		showPopup: true,
 		showOnIE: false,
 		headerTitle: 'Welcome!',
 		headerColor: '#128C7E',
 		backgroundColor: '#25d366',
-		buttonImage: '<img src="../images/whatsapp.svg" />'
+		buttonImage: '<img src="../images/whatsapp.svg"/>'
+	});
+
+	$('#myButton1').floatingWhatsApp({
+		phone: '+917058684314',
+		popupMessage: 'Hello, how can I help you?',
+		message: "I'd like to join ",
+		showPopup: true,
+		showOnIE: false,
+		headerTitle: 'Welcome!',
+		headerColor: '#128C7E',
+		backgroundColor: '#25d366',
+		buttonImage: '<img src="./images/whatsapp.svg"/>'
 	});
 });
 
